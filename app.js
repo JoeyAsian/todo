4d61e65bf15663c88b6fc2ce3b8d9059959b4b17
Joseph <firepheonix1914@gmail.com>
2019-06-27 14:40:49 -0400

    commiting before we go home

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express')
-const app = express();
-//seting template engine
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended:false})
 
+const dummyData = [];
+
+//setting up
+const app = express();
+
+
+//seting template engine
 app.set("view engine","ejs");
+
 
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+
+//############# ROUTES ################
+
+//Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
   });
+
+//Post for tasks: posting a task
+app.post('/tasks', urlEncoded, function(req, res){
+
+  dummyData.push(req.body.task)
+  console.log(dummyData)
+
+});
+
 
   app.listen(3000, function(err){
     if (err)
