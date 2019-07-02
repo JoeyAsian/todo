502ab696aa972c4a9ee4787a4eb98e0283fa9151
Joseph <firepheonix1914@gmail.com>
2019-07-02 10:01:48 -0400

    adding inout field as a require field so they cant input a task without text

--- app.js
+++ app.js
@@ -17,36 +17,36 @@ app.use(express.static('./public'));
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
-app.get('/tasks', function (req, res) {
-    console.log("hitting get route");
+app.get('/', (req, res) => {
+    // rendering tasks view and passing taskToDo data
     res.render('tasks', {taskToDo: dummyData});
 });
 
 // Post for tasks: posting a task
-app.post('/tasks', urlEncoded, function(req, res){
+app.post('/tasks', urlEncoded, (req, res)=> {
+    // formatting for incoming data to add to my data set
   console.log("hitting Post route");
-  let incomingItem = {}
-  incomingItem.taskItem = req.body.task
+  let incomingItem = {};
+  incomingItem.taskItem = req.body.task;
   dummyData.push(incomingItem)
-  console.log(dummyData)
-  res.redirect('/tasks')
+  res.redirect('/')
 });
 
-app.delete("/tasks/:id", function(req, res){
+app.delete("/tasks/:id", (req, res) => {
     // console.log(req.params.id);
     dummyData.splice(req.params.id, 1);
     // console.log(dummyData);
     res.json(dummyData)
 });
 
-app.delete("/tasks/:id", function(req, res){
-    //console.log(req.params.id);
+// Delete for task: deleting specific task
+app.delete("/tasks/:id", (req, res) => {
+    // deleting item from data set
     dummyData.splice(req.params.id, 1);
-    // console.log(dummyData);
     res.redirect(dummyData)
 })
 
-app.listen(3000, function(err){
+app.listen(3000, (err) =>{
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')
