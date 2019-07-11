5fbfb4a0b129e6c73829117a5d087a3a32e84f7e
Joseph <firepheonix1914@gmail.com>
2019-07-11 14:55:10 -0400

    gimme the points

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const routes = require ('./routes');
-const database = require ('./db')
+const express  = require('express');
+const routes   = require ('./routes');
+const database = require("./models");
 require('dotenv').config();
 
-const db = require("./models");
 
 const port = process.env.PORT || 3000 ;
 
@@ -26,7 +25,7 @@ app.set("view engine","ejs");
 app.use(express.static('./public'));
 
 
-db.sequelize.sync().then(function(){
+database.sequelize.sync().then(function(){
     app.listen(port, (err) =>{
         if (err)
         console.log(err)
