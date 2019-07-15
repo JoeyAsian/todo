0933a65976941cd83c1d66d9c49f3c5be9454e3d
Joseph <firepheonix1914@gmail.com>
2019-07-15 11:16:02 -0400

    i messed up

--- models/users.js
+++ models/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 // creating our task model
 // setting to export becauseout index file is expecting an export of the model
 module.exports = function(sequelize, DataTypes){
@@ -14,9 +16,21 @@ module.exports = function(sequelize, DataTypes){
             type: DataTypes.STRING,
             allowNull: false
         }
-        }
+    });
 
-    );
+    // create custom method for our user model
+    // this will handle if an unhashed password the user is providing
+    // is equal to the hash password in our database
+    User.prototype.validPassword = function(password){
+        return bcrypt.compareSync(password, this.password)
+    }
 
-    return User
+    // hooks are various methods that run during our model lifecycle
+    // in this case, before a user is created, we will automatically
+    // encrypt the password coming in
+    User.addhook('beforeCreate', function(user){
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+    });
+
+    return User;
 }
